c751340f1364ba7c01f0dfb19b50e4827114a17d
nickskywalkerjr <118694010+nickskywalkerjr@users.noreply.github.com>
2025-11-20 20:51:34 -0500

    Update __init__.py

--- custom_components/inkbird_wifi/__init__.py
+++ custom_components/inkbird_wifi/__init__.py
@@ -1,1 +1,25 @@
+"""ink bird_wifi integration."""
+import logging
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "ink bird_wifi"
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """From Tuya WiFi Scanner config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    
+    # Store the scan results
+    hass.data[DOMAIN][entry.entry_id] = entry.data.get("devices", [])
+    
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
