579a68fdd435b5b40c35052893ec28a4b96109e9
nickskywalkerjr <118694010+nickskywalkerjr@users.noreply.github.com>
2025-11-20 20:54:25 -0500

    Create const.py

---
+++ custom_components/inkbird_wifi/const.py
@@ -0,0 +1,17 @@
+"""Constants for the inkbird_wifi integration."""
+
+# Domain
+DOMAIN = "inkbird_wifi"
+
+# Configuration
+CONF_DEVICE_ID = "device_id"
+CONF_LOCAL_KEY = "local_key"
+CONF_IP_ADDRESS = "ip"
+CONF_PROTOCOL_VERSION = "version"
+
+# Default values
+DEFAULT_SCAN_INTERVAL = 30
+DEFAULT_PROTOCOL_VERSION = "3.3"
+
+# Platforms
+PLATFORMS = ["sensor"]
